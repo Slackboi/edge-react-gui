36fe2fc64d74f3e6823ca7255e64e13e1fb2d0ef
Captain Crypto <kylan.hurt@gmail.com>
2018-08-22 19:08:50 -0700

    WIP get fiat showing aligned-right

--- src/modules/UI/scenes/Scan/Scan.ui.js
+++ src/modules/UI/scenes/Scan/Scan.ui.js
@@ -3,7 +3,6 @@
 import React, { Component } from 'react'
 import { ActivityIndicator, Text, TouchableHighlight, View } from 'react-native'
 import { RNCamera } from 'react-native-camera'
-
 // $FlowFixMe
 import ImagePicker from 'react-native-image-picker'
 import { Actions } from 'react-native-router-flux'
@@ -92,7 +91,9 @@ export default class Scan extends Component<Props> {
             </View>
             <ABAlert />
           </View>
-          {this.props.showToWalletModal && <WalletListModal topDisplacement={Constants.SCAN_WALLET_DIALOG_TOP} type={Constants.FROM} />}
+          {this.props.showToWalletModal && (
+            <WalletListModal topDisplacement={Constants.SCAN_WALLET_DIALOG_TOP} type={Constants.FROM} onSelectWallet={onSelectWallet} />
+          )}
         </View>
 
         <LegacyAddressModal continueButtonPressed={legacyAddressModalContinueButtonPressed} cancelButtonPressed={legacyAddressModalCancelButtonPressed} />
@@ -138,7 +139,9 @@ export default class Scan extends Component<Props> {
     if (this.props.cameraPermission === AUTHORIZED) {
       const torchMode = this.props.torchEnabled ? RNCamera.Constants.FlashMode.on : RNCamera.Constants.FlashMode.off
 
-      return <RNCamera style={styles.preview} type={RNCamera.Constants.Type.back} ref="cameraCapture" flashMode={torchMode} onBarCodeRead={this.onBarCodeRead} />
+      return (
+        <RNCamera style={styles.preview} type={RNCamera.Constants.Type.back} ref="cameraCapture" flashMode={torchMode} onBarCodeRead={this.onBarCodeRead} />
+      )
     } else if (this.props.cameraPermission === DENIED) {
       return (
         <View style={[styles.preview, { justifyContent: 'center', alignItems: 'center' }]}>
